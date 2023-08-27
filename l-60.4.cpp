4f6a6a747ea81ff86ad546b8a55cb85e055f4d94
romeokil <123149490+romeokil@users.noreply.github.com>
2023-08-28 00:39:18 +0530

    Creating/Building Tree using levelorder Traversal
    level order Traversal ka use krke hmlog aaram se tree ko build kr skte hai. jaise ki normal method se kiye the(wo recursion wala)

--- l-60.4.cpp
+++ l-60.4.cpp
@@ -1,17 +1,18 @@
 //This is lecture no 60.4.cp
 //Deque implementation using stl.
+//For Deque more operation refer documentation of cplusplus.
 #include<iostream>
 #include<queue>
 using namespace std;
 int main(){
 	deque<int>dq;
-	dq.push_front(2);
-	dq.push_back(3);
+	dq.push_front(2);//Pushing the element in the front
+	dq.push_back(3);//Pushing the element in the back
 	cout<<"Front element in the queue is "<<dq.front()<<endl;
 	cout<<"Rear element in the queue is "<<dq.back()<<endl;
-	dq.pop_front();
-	dq.pop_back();
-	if(dq.empty()){
+	dq.pop_front(); //popping the element from the front
+	dq.pop_back();  //popping the element from the back
+	if(dq.empty()){ //Checking deque is empty or not
 		cout<<"Dequeue is empty"<<endl;
 	}
 	else{
